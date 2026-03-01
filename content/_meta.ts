b56cb17f329df0bef209cde549ac93eb24bfe6d0
Parvat Raj Singh <parvat.raj2@gmail.com>
2026-03-01 20:04:00 +0530

    docs: add content root meta

---
+++ content/_meta.ts
@@ -0,0 +1,10 @@
+export default {
+  "get-started": "Get Started",
+  examples: "Examples",
+  concepts: "Core Concepts",
+  api: "API Reference",
+  guides: "Guides",
+  operations: "Operations",
+  security: "Security",
+  project: "Project",
+};

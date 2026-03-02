98b1f812309b2e2e6155568b204f77fd8bb6dca9
Parvat Raj Singh <parvat.raj2@gmail.com>
2026-03-02 16:08:00 +0530

    docs(concepts): add architecture

---
+++ content/concepts/_meta.ts
@@ -0,0 +1,6 @@
+export default {
+  architecture: "Architecture",
+  "wallets-keypairs": "Wallets and Keypairs",
+  "instructions-transactions": "Instructions and Transactions",
+  "serialization-signing": "Serialization and Signing",
+};

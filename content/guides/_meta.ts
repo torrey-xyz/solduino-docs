bf32a0748d163e17fd4049587ed5890f75776435
Parvat Raj Singh <parvat.raj2@gmail.com>
2026-03-05 01:47:00 +0530

    docs(guides): add airdrop and transfer

---
+++ content/guides/_meta.ts
@@ -0,0 +1,6 @@
+export default {
+  "airdrop-flow": "Airdrop Flow",
+  "sol-transfer": "SOL Transfer",
+  "custom-programs": "Custom Program Interactions",
+  "sensor-to-chain": "Sensor-to-Chain Pattern",
+};
